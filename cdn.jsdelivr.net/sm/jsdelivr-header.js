8aed7f360772046bc3eb0e133b11111ae5168902
fabyojhon <fabiano.santos.luiz@gmail.com>
2026-02-15 00:12:08 -0300

    peersonalize criativo

---
+++ cdn.jsdelivr.net/sm/jsdelivr-header.js
@@ -0,0 +1,6 @@
+/**
+ * Minified by jsDelivr using Terser v5.37.0.
+ * Original file: /npm/bootstrap-input-spinner@1.10.14/src/bootstrap-input-spinner.js
+ *
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
